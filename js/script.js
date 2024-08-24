1d6f2dd4dac94aa927d2e735af50d59c1634a8c4
Rongg-com <nourchomrong.biu@gmail.com>
2024-08-24 21:33:04 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ function Sidebar() {
     var content = document.querySelector('.btn');
 
     if (sidebar.style.left === '0px') {
+        sidebar.style.display = 'block';
         sidebar.style.left = '-250px';
         content.style.marginLeft = '0';
     } else {
+        sidebar.style.display = 'block';
         sidebar.style.left = '0';
         content.style.marginLeft = '60px'; 
     }
